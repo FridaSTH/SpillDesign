9e863683e6779875b1a097814dfbcf482bd59337
TommyChan6 <tommychan661995@gmail.com>
2024-10-12 23:56:52 +0200

    fix monster overlapping bug, add animation to pause menu

--- Assets/Scrips/PauseMenu.cs
+++ Assets/Scrips/PauseMenu.cs
@@ -2,17 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
+using System.Threading.Tasks;
 
 public class PauseMenu : MonoBehaviour
 {
     [SerializeField] GameObject pauseMenu;
 
+    [SerializeField] RectTransform pauseTitlePanel;
+    [SerializeField] float pauseTitleYPosBegin, pauseTitleYPosEnd;
+
+    [SerializeField] RectTransform pauseContentPanel;
+    [SerializeField] float pauseContentYPosBegin, pauseContentYPosEnd;
+
+    [SerializeField] float tweenDuration;
+
     public void PauseGame() {
         pauseMenu.SetActive(true);
         Time.timeScale = 0;
+        PauseIntro();
     }
 
-    public void ResumeGame() {
+    public async void ResumeGame() {
+        await PauseOutro();
         pauseMenu.SetActive(false);
         Time.timeScale = 1;
     }
@@ -30,4 +42,16 @@ public class PauseMenu : MonoBehaviour
     public void ExitGame() {
         Application.Quit();
     }
+
+    public void PauseIntro() {
+        pauseTitlePanel.DOAnchorPosY(pauseTitleYPosEnd, tweenDuration).SetUpdate(true);
+        pauseContentPanel.DOAnchorPosY(pauseContentYPosEnd, tweenDuration).SetUpdate(true);
+    }
+
+    async Task PauseOutro() {
+        var pauseTitleTween = pauseTitlePanel.DOAnchorPosY(pauseTitleYPosBegin, tweenDuration).SetUpdate(true).AsyncWaitForCompletion();
+        var pauseContentTween = pauseContentPanel.DOAnchorPosY(pauseContentYPosBegin, tweenDuration).SetUpdate(true).AsyncWaitForCompletion();
+        await Task.WhenAll(pauseTitleTween, pauseContentTween);
+    }
+
 }
