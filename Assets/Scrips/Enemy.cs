a33a7c857ba9adbe453e029d5aedd03a16d18f9c
TommyChan6 <tommychan661995@gmail.com>
2024-10-23 08:28:30 +0200

    add sprinter

--- Assets/Scrips/Enemy.cs
+++ Assets/Scrips/Enemy.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Enemy : MonoBehaviour
 {
@@ -22,9 +23,7 @@ public class Enemy : MonoBehaviour
     private bool isDying = false;
     public Collider2D hitBox;
     private SpriteRenderer spriteRenderer;
-
-
-
+    
     
     
 
@@ -59,8 +58,15 @@ public class Enemy : MonoBehaviour
         }
     }
 
+    private Vector2 currentMoveDirection;
+    public bool isDashable = false;
+    private bool isDashing = false;
+    private bool isStartDashing = false;
+    private float dashTimer = 0f;
+    public float dashSpeed = 1.5f;
+    public float dashTiggerRadius = 2f;
     // Update is called once per frame
-    private void FixedUpdate() {
+    private void Update() {
         // Find player and move towards the player
         if (isDying != false) {
             animator.SetBool("isMoving", false);
@@ -68,15 +74,52 @@ public class Enemy : MonoBehaviour
             movementInput = new Vector2(player.transform.position.x - transform.position.x, player.transform.position.y - transform.position.y);
             movementInput.Normalize();
             if (movementInput != Vector2.zero){
-                bool success = TryMove(movementInput);
+                // Dash with a random chance
+                if (isDashable) {
+                    if (
+                        Math.Sqrt((player.transform.position.x - transform.position.x)*(player.transform.position.x - transform.position.x)
+                        +((player.transform.position.y - transform.position.y)*(player.transform.position.y - transform.position.y))) < dashTiggerRadius
+                    ) {
+                        // randomValue = Random.Range(0f, 1f);
+                        // if (randomValue < 0.01)
+                        if (!isDashing) {
+                            StartChargingToDash();
+                            moveSpeed = 0f;
+                        }
+                        
+                    }
+                }
+                
+                if(isDashing) {
+                    dashTimer += Time.deltaTime;
+                    if(isStartDashing) {
+                        print("start actual");
+                        currentMoveDirection = movementInput;
+                        isStartDashing = false;
+                        moveSpeed = 4f;
+                    } else {
+                        // currentMoveDirection remains the same
+                        if(dashTimer >= 1f) {
+                            // End of dash
+                            isDashing = false;
+                            moveSpeed = 0.6f;
+                            dashTimer = 0f;
+                        } else {
+                            //rb.MovePosition(rb.position + direction * dashSpeed * Time.fixedDeltaTime);
+                        }
+                    }
+                } else {
+                    currentMoveDirection = movementInput;
+                }
+                bool success = TryMove(currentMoveDirection);
                 if(!success) {
-                    success = TryMove(new Vector2(movementInput.x, 0));
+                    success = TryMove(new Vector2(currentMoveDirection.x, 0));
                     if(!success) {
-                        success = TryMove(new Vector2(0, movementInput.y));
+                        success = TryMove(new Vector2(0, currentMoveDirection.y));
                     }
                 }
                 animator.SetBool("isMoving", success);   // Need to add more moving directions
-                if (movementInput.x < 0f) {
+                if (currentMoveDirection.x < 0f) {
                     spriteRenderer.flipX = true;
                 } else {
                     spriteRenderer.flipX = false;
@@ -124,10 +167,10 @@ public class Enemy : MonoBehaviour
 
     public void RemoveEnemy() {
         print("remove enemy");
-        if (dropRate > Random.Range(0f, 1f)) {
+        if (dropRate > UnityEngine.Random.Range(0f, 1f)) {
             if (dropList.Count > 0)
             {
-                int randomIndex = Random.Range(0, dropList.Count); // Get a random index
+                int randomIndex = UnityEngine.Random.Range(0, dropList.Count); // Get a random index
                 GameObject selectedObject = dropList[randomIndex]; // Select the GameObject
                 Instantiate(selectedObject, transform.position, Quaternion.identity);
             }
@@ -152,6 +195,40 @@ public class Enemy : MonoBehaviour
             //         notAttacked = false;
             // }
             // attack.GetComponent<Collider2D>().enabled = false;
+        }
+    }
+
+
+    
+    // public void DashForward(Vector2 direction) {
+    //     dashTimer += Time.deltaTime;
+    //     if(dashTimer >= 1f) {
+    //         // End dash
+    //         isDashing = false;
+    //     } else {
+    //         rb.MovePosition(rb.position + direction * dashSpeed * Time.fixedDeltaTime);
+    //     }
+    // }
+    public void StartChargingToDash() {
+        animator.SetBool("isCharging", true);
+    }
+
+    public void EndChargingToDash() {
+        animator.SetBool("isCharging", false);
+    }
+
+    public void StartDashing() {
+        if(isDashable) {
+            //print("start dashing");
+            if (!isDashing) {
+                print("start dashing");
+                isDashing = true;
+                isStartDashing = true;
+            }
+        } else {
+            print("Not able to dash, change the isDashable variable to make this enemy dashable");
+            isDashing = false;
+            isStartDashing = false;
         }
     }
 }
